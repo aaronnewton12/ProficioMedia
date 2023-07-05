2deb51189c8e0943bf4686a98e50fab2c0399dd6
Aaron Newton <aaron.12.newton@gmail.com>
2023-07-05 15:34:13 +0100

    Progress

--- symfony/src/Controller/GuidesController.php
+++ symfony/src/Controller/GuidesController.php
@@ -16,9 +16,7 @@ class GuidesController extends BaseController
         $this->guideUrls = array(
             'what-is-a-business-valuation',
             'how-do-you-calculate-the-value-of-a-business',
-            'how-do-you-increase-the-value-of-a-business',
-            'what-effects-the-value-of-a-business',
-            'why-do-i-need-to-know-the-value-of-my-business',
+            'how-do-you-increase-the-value-of-a-business'
         );
     }
 
@@ -62,7 +60,13 @@ class GuidesController extends BaseController
 
         if ($slug === 'what-is-a-business-valuation') {
             $metaTitle = 'What is a business valuation? | Business Valuations';
-            $metaDescription = 'A business valuation is a series of calculations against your businesses key metrics to give your business a saleable value';
+            $metaDescription = 'A business valuation is a series of calculations against your businesses key metrics to give your business a saleable value.';
+        } else if ($slug === 'how-do-you-calculate-the-value-of-a-business') {
+            $metaTitle = 'How do you calculate the value of a business? | Business Valuations';
+            $metaDescription = 'Discover how to calculate the value of a business with expert insights and methods. Learn about financial analysis, market trends, and more.';
+        } else if ($slug === 'how-do-you-increase-the-value-of-a-business') {
+            $metaTitle = 'How do you increase the value of a business? | Business Valuations';
+            $metaDescription = 'Unlock the secrets to increasing your business\'s value. Explore strategies, optimization techniques, and industry insights to boost your company\'s worth.';
         } else {
             $metaTitle = 'Free Business Guides | Business Valuations';
             $metaDescription = 'Expert, jargon-free guides to help you understanding growing, valuing and selling a business.';
